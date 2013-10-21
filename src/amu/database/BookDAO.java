53f8c3e9530f40ccd55ce78435379477256fc0bb
simen skoglund <simen.skoglund@gmail.com>
2013-10-21 19:49:37 +0200

    Endret alle databaseklassene til preparedstatements fra vanlig statements

--- src/amu/database/BookDAO.java
+++ src/amu/database/BookDAO.java
@@ -8,23 +8,23 @@ import java.sql.*;
 import java.util.logging.*;
 
 public class BookDAO {
+	Book book = null;
+	
+	Connection connection = null;
+	PreparedStatement statement = null;
+	ResultSet resultSet = null;
     public Book findByISBN(String isbn) {
-        Book book = null;
-        
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
         
         try {
             connection = Database.getConnection();
-            statement = connection.createStatement();
             
             String query = "SELECT * FROM book, publisher, title "
-                    + "WHERE book.isbn13 = '"
-                    + isbn + "' "
+                    + "WHERE book.isbn13 = ? "
                     + "AND book.title_id = title.id "
                     + "AND book.publisher_id = publisher.id;";
-            resultSet = statement.executeQuery(query);
+            statement = connection.prepareStatement(query);
+            statement.setString(1, isbn);
+            resultSet = statement.executeQuery();
             Logger.getLogger(this.getClass().getName()).log(Level.FINE, "findByISBN SQL Query: " + query);
             
             if (resultSet.next()) {
@@ -56,19 +56,19 @@ public class BookDAO {
         Book book = null;
         
         Connection connection = null;
-        Statement statement = null;
+        PreparedStatement statement = null;
         ResultSet resultSet = null;
         
         try {
             connection = Database.getConnection();
-            statement = connection.createStatement();
             
             String query = "SELECT * FROM book, publisher, title "
-                    + "WHERE book.id = '"
-                    + id + "' "
+                    + "WHERE book.id = ? "
                     + "AND book.title_id = title.id "
                     + "AND book.publisher_id = publisher.id;";
-            resultSet = statement.executeQuery(query);
+            statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+            resultSet = statement.executeQuery();
             Logger.getLogger(this.getClass().getName()).log(Level.FINE, "findByISBN SQL Query: " + query);
             
             if (resultSet.next()) {
