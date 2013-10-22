6a6018d4812f0df14ded9d93ee907e8c3cba21d8
chrpeter <christian150790@gmail.com>
2013-10-22 13:48:30 +0200

    added error handling for wrong urls

---
+++ WebContent/404.jsp
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1>404 Not found</h1>
+    <h2>This site doesnt exist, what are you up to?</h2>
+    <div class="index-item"><a href="<c:url value="/" />">Go back to front page</a></div>
+</div>
--- src/amu/FrontController.java
+++ src/amu/FrontController.java
@@ -4,7 +4,10 @@ import amu.action.Action;
 import amu.action.ActionFactory;
 import amu.action.ActionResponse;
 import amu.action.ActionResponseType;
+import amu.model.ErrorMessage;
+
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,22 +15,35 @@ import javax.servlet.http.HttpServletResponse;
 
 public class FrontController extends HttpServlet {
 
-    @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        try {
-            Action action = ActionFactory.getAction(request);
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {				System.out.println("halla1");
 
-            ActionResponse actionResponse = action.execute(request, response);
-            
-            if (actionResponse.getType() == ActionResponseType.REDIRECT) {
-                response.sendRedirect(actionResponse.getURL() + actionResponse.getParameterString());
-            } else { // actionResponse.getType() == ActionResponse.Type.FORWARD
-                request.getRequestDispatcher(actionResponse.getURL()).forward(request, response);
-            }
-            
-        } catch (Exception e) {
-            throw new ServletException("Executing action failed.", e);
-        }
-    }
+			Action action = ActionFactory.getAction(request);
+			System.out.println("halla2");
+
+			//If a user types in a page that does not exist.
+			if(action == null){
+				System.out.println("test");
+				ErrorMessage error = new ErrorMessage("404 Not found", "The page you requested doesnt exist");
+				request.setAttribute("errorMessage", error);
+				request.getRequestDispatcher("/generalErrorMessage.jsp").forward(request, response);
+			}
+			else {
+				ActionResponse actionResponse = action.execute(request, response);
+				
+				if (actionResponse.getType() == ActionResponseType.REDIRECT) {
+					response.sendRedirect(actionResponse.getURL() + actionResponse.getParameterString());
+				} else { // actionResponse.getType() == ActionResponse.Type.FORWARD
+					request.getRequestDispatcher(actionResponse.getURL()).forward(request, response);
+				}
+			}
+
+		} catch (Exception e) {
+			ErrorMessage error = new ErrorMessage("404 Not found", "The page you requested doesnt exist");
+			request.setAttribute("errorMessage", error);
+			request.getRequestDispatcher("/generalErrorMessage.jsp").forward(request, response);
+		}
+	}
 }
