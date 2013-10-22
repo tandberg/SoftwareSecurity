eb586b95bf6b53d7071286f3ba9353bb22374a7f
simen skoglund <simen.skoglund@gmail.com>
2013-10-22 13:44:23 +0200

    Fikset at man får en melding om man prøver å aktivere flere ganger på rad. Og fikset at man ikke kan oppdatere handlevogn med negative tall eller bokstaver

--- src/amu/action/ActivateCustomerAction.java
+++ src/amu/action/ActivateCustomerAction.java
@@ -2,35 +2,45 @@ package amu.action;
 
 import amu.database.CustomerDAO;
 import amu.model.Customer;
+import amu.model.ErrorMessage;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 class ActivateCustomerAction implements Action {
 
-    @Override
-    public ActionResponse execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	@Override
+	public ActionResponse execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        // If email field is set, but token is not provided then this is a idempotent request
-        if (request.getParameter("email") != null && request.getParameter("activationToken") == null) {
-            request.setAttribute("email", request.getParameter("email"));
-        }
+		// If email field is set, but token is not provided then this is a idempotent request
+		if (request.getParameter("email") != null && request.getParameter("activationToken") == null) {
+			request.setAttribute("email", request.getParameter("email"));
+		}
 
-        // If both fields are set, then this is a non-idempotent activation request
-        if (request.getParameter("email") != null && request.getParameter("activationToken") != null) {
-            CustomerDAO customerDAO = new CustomerDAO();
-            Customer customer = customerDAO.findByEmail(request.getParameter("email"));
+		// If both fields are set, then this is a non-idempotent activation request
+		if (request.getParameter("email") != null && request.getParameter("activationToken") != null) {
+			CustomerDAO customerDAO = new CustomerDAO();
+			Customer customer = customerDAO.findByEmail(request.getParameter("email"));
+			
+			/*Check if a user is already activated and create an error response if it is.*/
+			if (customer != null && customer.getActivationToken() == null){
+				ErrorMessage error = new ErrorMessage("Customer Already Activated", "This customer is already activated, please log in.");
+				request.setAttribute("errorMessage", error);
+				return new ActionResponse(ActionResponseType.FORWARD, "generalErrorMessage");
+			}
+			
+			else if (customer != null && customer.getActivationToken().equals(request.getParameter("activationToken"))) {
+				customer = customerDAO.activate(customer);
+				return new ActionResponse(ActionResponseType.REDIRECT, "activationSuccessful");
+			}
+			else {
+				HttpSession session = request.getSession(true);
+				session.setAttribute("debugActivation", customer);
+				return new ActionResponse(ActionResponseType.REDIRECT, "activationError");
+			}
+		}
 
-            if (customer != null && customer.getActivationToken().equals(request.getParameter("activationToken"))) {
-                customer = customerDAO.activate(customer);
-                return new ActionResponse(ActionResponseType.REDIRECT, "activationSuccessful");
-            } else {
-                HttpSession session = request.getSession(true);
-                session.setAttribute("debugActivation", customer);
-                return new ActionResponse(ActionResponseType.REDIRECT, "activationError");
-            }
-        }
-        
-        return new ActionResponse(ActionResponseType.FORWARD, "activateCustomer");
-    }
+		return new ActionResponse(ActionResponseType.FORWARD, "activateCustomer");
+	}
 }
